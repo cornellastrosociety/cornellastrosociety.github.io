2af1cbc46f8d2fc012d308f0737c0bbfd8fc9358
ail36 <ail36@cornell.edu>
2024-06-29 12:12:44 -0400

    Move header and footer to js files

---
+++ src/data/js/footer.js
@@ -0,0 +1,24 @@
+var footer_data = `<div style="padding: 2vh;"></div>
+
+<footer style="background-color: rgba(0, 0, 0, 0.8); left: 0; width: 100vw; align-items: center; justify-content: center; font-size: 0.8em;">
+    <div style="padding: 2vh;"></div>
+    <div class="main_content" style="display: grid; grid-template-columns: 4fr 2.2fr 1px 2fr 1px 1.5fr 1px 3fr 1px 2fr 4fr; margin: auto;">
+        <div></div>
+        <div style="text-align: left; padding: 1rem 10%; overflow-wrap: anywhere;">Email us: <a href="mailto:astrosociety@cornell.edu">astrosociety@cornell.edu</a></div>
+        <div style="background-color: white; width: 100%;"></div>
+        <div style="text-align: left; padding: 1rem 10%;">Find us:<br>209 Cradit Farm Drive<br>Ithaca, New York 14850</div>
+        <div style="background-color: white; width: 100%;"></div>
+        <div style="text-align: left; padding: 1rem 10%;">Hours:<br>Fri: 8pm - 12am</div>
+        <div style="background-color: white; width: 100%;"></div>
+        <div id="copyright" style="text-align: center; padding: 1rem 10%; font-size: 0.8em; line-height: 1.5em;">© 2024 by the Cornell Astronomical Society<br>Member Login<br><br>This organization is a registered student organization of Cornell University.</div>
+        <div style="background-color: white; width: 100%;"></div>
+        <div style="padding: 1rem 10%; max-width: 20vw; display: grid; grid-template-columns: 1fr 1fr 1fr; gap: 5%; place-items: center;">
+            <a href="https://www.facebook.com/FuertesObservatory"><img style="width: 100%;" src="data/social/Facebook_Logo_Primary.png"></a>
+            <a href="https://twitter.com/FuertesObs"><img style="width: 100%;" src="data/social/Logo_of_Twitter.png"></a>
+            <a href="https://www.instagram.com/fuertesobservatory"><img style="width: 100%;" src="data/social/Instagram_Glyph_Gradient.png"></a>
+        </div>
+        <div></div>
+    </div>
+    <div style="padding: 2vh;"></div>`;
+
+document.write(footer_data);
