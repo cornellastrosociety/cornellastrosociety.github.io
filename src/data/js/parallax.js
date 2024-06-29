a3e0cf5ff59675ba033b3a5792f600178df830b5
therealhashimbhatnagar <109086488+therealhashimbhatnagar@users.noreply.github.com>
2024-06-29 01:23:11 -0400

    outerwidth, not innerwidth

--- src/data/js/parallax.js
+++ src/data/js/parallax.js
@@ -1,8 +1,10 @@
 window.onresize = function () {
-    var windowsize = window.innerHeight;
+
+    var windowsize = window.outerHeight;
+    var windowwide = window.outerWidth;
     var height = document.getElementById('test').scrollHeight;
     let shift = ((Math.abs(height - windowsize) + (height - windowsize)) / 4);
-    let scale = windowsize + ((Math.abs(height - windowsize) + (height - windowsize)) / 185)
+    let scale = windowsize + ((Math.abs(height - windowsize) + (height - windowsize)) / 198)
     document.getElementById('bg').style.marginTop = (shift) + "px"
     document.getElementById('bg').style.height = scale + "px";
 
@@ -14,26 +16,27 @@ window.onresize = function () {
     if (scrolldist > height - windowsize) {
         document.getElementById('wind').scrollTo(0, height - windowsize);
     }
+
 }
 
 window.onload = function () {
-    var windowsize = window.innerHeight;
-    var height = document.getElementById('test').scrollHeight;
-    let shift = ((Math.abs(height - windowsize) + (height - windowsize)) / 4);
-    let scale = windowsize + ((Math.abs(height - windowsize) + (height - windowsize)) / 185)
-    document.getElementById('bg').style.marginTop = (shift) + "px"
-    document.getElementById('bg').style.height = scale + "px";
 
+
+    var windowsize = window.outerHeight;
+    var windowwide = window.outerWidth;
+    var height = document.getElementById('test').scrollHeight;
+
+    let shift = ((Math.abs(height - windowsize) + (height - windowsize)) / 4);
+    let scale = windowsize + ((Math.abs(height - windowsize) + (height - windowsize)) / 198     )
+    document.getElementById('bg').style.marginTop = shift + "px";
+    document.getElementById('bg').style.height = scale + "px";
+    document.getElementById('bg').style.width = windowwide + "px";
     setTimeout(() => {
         document.getElementById('bg').style.marginTop = (shift - 10) + "px";
     },
         10);
-    var scrolldist = document.getElementById('wind').scrollTop;
-    if (scrolldist > height - windowsize) {
-        document.getElementById('wind').scrollTo(0, height - windowsize);
-    }
-}
 
+}
 function overscroll() {
     var windowsize = window.innerHeight;
     var windowwide = window.innerWidth;
