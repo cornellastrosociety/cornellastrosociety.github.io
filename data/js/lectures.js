d466edca7c4c861d84b3e4da72c4b22d3079eb72
ail36 <ail36@cornell.edu>
2024-07-02 11:18:16 -0400

    Artificial delay to lectures page to improve general loading time

---
+++ data/js/lectures.js
@@ -0,0 +1,18 @@
+/*
+=~=~=~=~=~=~=~=~=
+Adding a lecture:
+Add the YouTube video ID to the end of the `ids` list below.
+
+=~=~=~=~=~=~=~=~=
+*/
+var ids = ['9WqaUSiB76k', 'dFpkdy4J43Q', 'Mn_lIMv8KCM', 'YG3vfEI_Loo', 'J4kRIdGJUco', 'BdOLtSz99Aw', 'ct9PMMnYkpM', 'dSt0EE6suQA', 'U7-tVG1SUFw', '_1tdOcud7F8', 'kjH2Tt7rgIM', 'YTSHpk7JPCI', 'jzy7t3nIxD8', 'nQ-kMP00Q08', 'H08XRPi0DP4', 'ZgY4K0plnhY', '4AXuRbtHvBE', 'lO2UUDqPGbQ', 'MkBb9lhTr1s', 'JVuZ8vnSuuw', 'zV6DL-EHSUQ', 'S6AxTlTcx3g', 'JVuZ8vnSuuw', '-e6ZgK-Yptg', 'eNr5pcTNrv0'];
+
+var iframes = document.getElementsByTagName('iframe');
+ids.reverse();
+
+for (let i = ids.length; i > 0; i--) {
+    setTimeout(() => {
+        iframes[i - 1].src = `https://www.youtube-nocookie.com/embed/${ids[i - 1]}`;
+    }, i * (5000 / ids.length));
+}
+// Load time fixed at 5 seconds, loading each iframe at a separate instant, as to improve overall page loading time
