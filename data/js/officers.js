cfb37924db60b7541f7ab2fef85d84e71542c835
ail36 <ail36@cornell.edu>
2024-07-03 01:49:00 -0400

    Make officer table dynamic based on Google sheet

---
+++ data/js/officers.js
@@ -0,0 +1,17 @@
+// Stolen from https://stackoverflow.com/questions/16485255
+var url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQn19DgNGfikvgpEcoEJBe4105Ng04R8JTtCwYNO1caZO87oZ1jCVMByCFmQ8qkk-kmlgDaUSIBbrZH/pub?output=csv";
+xmlhttp = new XMLHttpRequest();
+xmlhttp.onreadystatechange = function () {
+    if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+        var tab = xmlhttp.responseText.split(/\n/g);
+        var str = '';
+        tab.shift();
+        for (const row of tab) {
+            var els = row.split(/,/g);
+            str += `<tr><td>${els[0]}</td><td>${els[1]}</td><td>${els[2]}</td><td>${els[3]}</td><td>${els[4]}</td><td>${els[5]}</td></tr>`;
+        }
+        document.getElementById('tab_officers').innerHTML = str;
+    }
+};
+xmlhttp.open("GET", url, true);
+xmlhttp.send(null);
