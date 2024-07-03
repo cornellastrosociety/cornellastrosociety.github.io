2640c7cb79b16ce4574b88aa2828c92804f302a3
ail36 <ail36@cornell.edu>
2024-07-02 23:00:12 -0400

    General repairs to parallax

--- data/js/parallax.js
+++ data/js/parallax.js
@@ -1,7 +1,5 @@
 window.onresize = function () {
-
-    var windowsize = window.outerHeight;
-    var windowwide = window.outerWidth;
+    var windowsize = window.innerHeight;
     var height = document.getElementById('test').scrollHeight;
     let shift = ((Math.abs(height - windowsize) + (height - windowsize)) / 4);
     let scale = windowsize + ((Math.abs(height - windowsize) + (height - windowsize)) / 198)
@@ -16,30 +14,33 @@ window.onresize = function () {
     if (scrolldist > height - windowsize) {
         document.getElementById('wind').scrollTo(0, height - windowsize);
     }
-
 }
 
 window.onload = function () {
-
-
-    var windowsize = window.outerHeight;
+    var windowsize = window.innerHeight;
     var windowwide = window.outerWidth;
     var height = document.getElementById('test').scrollHeight;
-
     let shift = ((Math.abs(height - windowsize) + (height - windowsize)) / 4);
-    let scale = windowsize + ((Math.abs(height - windowsize) + (height - windowsize)) / 198     )
-    document.getElementById('bg').style.marginTop = shift + "px";
+    let scale = windowsize + ((Math.abs(height - windowsize) + (height - windowsize)) / 198)
+    document.getElementById('bg').style.marginTop = (shift) + "px";
     document.getElementById('bg').style.height = scale + "px";
+
+
     document.getElementById('bg').style.width = windowwide + "px";
+
+
     setTimeout(() => {
         document.getElementById('bg').style.marginTop = (shift - 10) + "px";
     },
         10);
-
+    var scrolldist = document.getElementById('wind').scrollTop;
+    if (scrolldist > height - windowsize) {
+        document.getElementById('wind').scrollTo(0, height - windowsize);
+    }
 }
+
 function overscroll() {
     var windowsize = window.innerHeight;
-    var windowwide = window.innerWidth;
     var height = document.getElementById('test').scrollHeight;
     var scrolldist = document.getElementById('wind').scrollTop;
     if (scrolldist > height - windowsize) {
