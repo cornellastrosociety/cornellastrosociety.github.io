821ee56d2c0c9ba47c8c4a8b73ab7e11742d095f
ail36 <ail36@cornell.edu>
2024-08-01 19:25:41 -0400

    Add meta image tags

---
+++ data/js/meta.js
@@ -0,0 +1,6 @@
+const content = `<meta property="og:image" content="https://new.cornellastrosociety.org/data/img/orion.jpg">
+<meta property="og:image:type" content="image/jpeg">
+<meta property="og:image:width" content="2340">
+<meta property="og:image:height" content="1677">`
+
+document.write(content);
