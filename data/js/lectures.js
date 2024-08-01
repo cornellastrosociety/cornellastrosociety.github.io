5c0e74ad14437b278ce07fa9a120bf7ec408bb51
ail36 <ail36@cornell.edu>
2024-08-01 14:22:02 -0400

    Revisions to scattered text and formatting

--- data/js/lectures.js
+++ data/js/lectures.js
@@ -5,7 +5,7 @@ Add the YouTube video ID to the end of the `ids` list below.
 
 =~=~=~=~=~=~=~=~=
 */
-var ids = ['9WqaUSiB76k', 'dFpkdy4J43Q', 'Mn_lIMv8KCM', 'YG3vfEI_Loo', 'J4kRIdGJUco', 'BdOLtSz99Aw', 'ct9PMMnYkpM', 'dSt0EE6suQA', 'U7-tVG1SUFw', '_1tdOcud7F8', 'kjH2Tt7rgIM', 'YTSHpk7JPCI', 'jzy7t3nIxD8', 'nQ-kMP00Q08', 'H08XRPi0DP4', 'ZgY4K0plnhY', '4AXuRbtHvBE', 'lO2UUDqPGbQ', 'MkBb9lhTr1s', 'JVuZ8vnSuuw', 'zV6DL-EHSUQ', 'S6AxTlTcx3g', 'JVuZ8vnSuuw', '-e6ZgK-Yptg', 'eNr5pcTNrv0'];
+var ids = ['9WqaUSiB76k', 'dFpkdy4J43Q', 'Mn_lIMv8KCM', 'YG3vfEI_Loo', 'J4kRIdGJUco', 'BdOLtSz99Aw', 'ct9PMMnYkpM', 'dSt0EE6suQA', 'U7-tVG1SUFw', '_1tdOcud7F8', 'kjH2Tt7rgIM', 'YTSHpk7JPCI', 'jzy7t3nIxD8', 'nQ-kMP00Q08', 'H08XRPi0DP4', 'ZgY4K0plnhY', '4AXuRbtHvBE', 'lO2UUDqPGbQ', 'MkBb9lhTr1s', 'za9UFwB1cKI', 'zV6DL-EHSUQ', 'S6AxTlTcx3g', 'JVuZ8vnSuuw', '-e6ZgK-Yptg', 'eNr5pcTNrv0'];
 
 var iframes = document.getElementsByTagName('iframe');
 ids.reverse();
