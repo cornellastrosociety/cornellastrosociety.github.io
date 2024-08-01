80a70adeea0df272774a902df12986adfb8ec1de
ail36 <ail36@cornell.edu>
2024-08-01 13:58:49 -0400

    Make code formatting consistent

--- data/js/lightbox.js
+++ data/js/lightbox.js
@@ -4,15 +4,13 @@ let slideIndexTwo = 1;
 showSlideTwo(slideIndexTwo);
 
 function openLightboxTwo() {
-    if (document.getElementById('hamburger') != null)
-    {
+    if (document.getElementById('hamburger') != null) {
         document.getElementById('hamburger').classList.add('hamburger_hide');
     }
-    else if (document.getElementById('burger') != null)
-    {
+    else if (document.getElementById('burger') != null) {
         document.getElementById('burger').classList.add('burger_hide');
     }
-   
+
 
     document.getElementById('lightboxtwo').style.display = 'block';
     document.getElementsByTagName('html')[0].style.overflow = 'hidden';
@@ -21,16 +19,14 @@ function openLightboxTwo() {
 };
 
 function closeLightboxTwo() {
-      if (document.getElementById('hamburger') != null)
-        {
-            document.getElementById('hamburger').classList.add('hamburger_show');
-            document.getElementById('hamburger').classList.remove('hamburger_hide');
-        }
-        else if (document.getElementById('burger') != null)
-        {
-            document.getElementById('burger').classList.add('burger_show');
-            document.getElementById('burger').classList.remove('burger_hide');
-        }
+    if (document.getElementById('hamburger') != null) {
+        document.getElementById('hamburger').classList.add('hamburger_show');
+        document.getElementById('hamburger').classList.remove('hamburger_hide');
+    }
+    else if (document.getElementById('burger') != null) {
+        document.getElementById('burger').classList.add('burger_show');
+        document.getElementById('burger').classList.remove('burger_hide');
+    }
     document.getElementById('lightboxtwo').style.display = 'none';
     document.getElementsByTagName('html')[0].style.overflow = 'auto';
     document.getElementsByTagName('body')[0].style.overflow = 'auto';
@@ -66,36 +62,32 @@ function showSlideTwo(n) {
     };
 
 
-   
+
     if (slidesTwo.length >= slideIndexTwo) slidesTwo[slideIndexTwo - 1].style.display = 'grid';
     if (modalPreviewsTwo.length >= slideIndexTwo) modalPreviewsTwo[slideIndexTwo - 1].className += ' active';
     resizeSlideTwo(slideIndexTwo);
 };
 
 function resizeSlideTwo(n) {
-    i = n-1;
-    imgH =  document.getElementsByClassName('slideTwo')[i].children[0].height;
-    imgW =  document.getElementsByClassName('slideTwo')[i].children[0].width;
+    i = n - 1;
+    imgH = document.getElementsByClassName('slideTwo')[i].children[0].height;
+    imgW = document.getElementsByClassName('slideTwo')[i].children[0].width;
     vpH = window.innerHeight;
     vpW = window.innerWidth;
-    if (document.getElementsByClassName('slideTwo')[i].children[0].height/document.getElementsByClassName('slideTwo')[i].children[0].width > ((5/6) * (vpH/vpW)))
-    {   
-        document.getElementsByClassName('slideTwo')[0].parentElement.style.width = (vpH * 7/10  * (imgW/imgH) )+ 'px';
+    if (document.getElementsByClassName('slideTwo')[i].children[0].height / document.getElementsByClassName('slideTwo')[i].children[0].width > ((5 / 6) * (vpH / vpW))) {
+        document.getElementsByClassName('slideTwo')[0].parentElement.style.width = (vpH * 7 / 10 * (imgW / imgH)) + 'px';
         document.getElementsByClassName('slideTwo')[0].parentElement.style.height = 'auto';
 
     }
-    else
-    {
-        document.getElementsByClassName('slideTwo')[0].parentElement.style.width = (vpW * 9/10 ) + 'px';
+    else {
+        document.getElementsByClassName('slideTwo')[0].parentElement.style.width = (vpW * 9 / 10) + 'px';
         document.getElementsByClassName('slideTwo')[0].parentElement.style.height = 'auto';
 
     }
-    if (document.getElementsByClassName('slideTwo')[i].childElementCount >= 2)
-    {
+    if (document.getElementsByClassName('slideTwo')[i].childElementCount >= 2) {
         document.getElementsByClassName('slideTwo')[i].children[1].style.fontSize = vpH / 40 + 'px';
     }
-    if (document.getElementsByClassName('slideTwo')[i].childElementCount >= 3)
-    {
+    if (document.getElementsByClassName('slideTwo')[i].childElementCount >= 3) {
         document.getElementsByClassName('slideTwo')[i].children[2].style.fontSize = vpH / 60 + 'px';
     }
 }
