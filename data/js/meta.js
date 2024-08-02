fffdb5e63c16f88f9267435fa080ecd024095622
ail36 <ail36@cornell.edu>
2024-08-02 00:55:25 -0400

    Update URL in preparation for publish

--- data/js/meta.js
+++ data/js/meta.js
@@ -1,4 +1,4 @@
-const content = `<meta property="og:image" content="https://new.cornellastrosociety.org/data/img/orion.jpg">
+const content = `<meta property="og:image" content="https://www.cornellastrosociety.org/data/img/orion.jpg">
 <meta property="og:image:type" content="image/jpeg">
 <meta property="og:image:width" content="2340">
 <meta property="og:image:height" content="1677">`
