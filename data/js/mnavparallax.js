1edd9eda047a9d0602a7a3e3d7237cc9453ef6ac
therealhashimbhatnagar <109086488+therealhashimbhatnagar@users.noreply.github.com>
2024-08-06 13:20:24 -0400

    increase padding for moonburger

--- data/js/mnavparallax.js
+++ data/js/mnavparallax.js
@@ -8,46 +8,51 @@ var nav_data = `<!-- Nav bar, should persist across pages; add nav_el_sel to cur
                     <div style = "height: 15vh"></div>
                     <div class = "no_und" ><a class = "no_und"   href="/"><div  class = "mnav_main" id="nav_home">Home</div></a> </div>
 
-                    <div  class = "mnav_main" id="nav_about">About <i class = "arrow down" id = "aboutarrow" onclick = showhide("about")> </i > </div>
-                        <div>
-                            <div class = "mnav_sub no_und"id = "mnav_about" style = "display: none; color: black">
-                                <a href="/about/theclub" id="nav_about_theclub" class = "nav_dd_sub">The Club <br></a>
-                                <a href="/about/telescope" id="nav_about_telescope" class = "nav_dd_sub">The Telescope <br></a>
-                                <a href="/about/fuertes" id="nav_about_fuertes" class = "nav_dd_sub">Fuertes History <br></a>
-                                <a href="/about/irvhistory" id="nav_about_irvhistory" class = "nav_dd_sub">Telescope History <br></a>
-                                <a href="/about/slides" id="nav_about_slides" class = "nav_dd_sub">Lantern Slide Wall <br></a>
-                            </div>
-                        </div>
-                  
-         
-                        <div class = "mnav_main"  id="nav_visit">Visit Us <i class = "arrow down" id = "visitarrow" onclick = showhide("visit")> </i ></div>
-                        <div >
-                            <div  class = "mnav_sub no_und" id = "mnav_visit" style = "display: none; color: black">
-                                <a href="/visit/hours" id="nav_visit_hours" class = "nav_dd_sub">Hours, Directions <br></a>
-                                <a href="/visit/accessibility" id="nav_visit_accessibility" class = "nav_dd_sub">Accessibility <br></a>
-                                <a href="/visit/weather" id="nav_visit_weather" class = "nav_dd_sub">Weather <br></a>
-                            </div>
-                        </div>
-        
-                        <div class = "mnav_main "  id="nav_events">Events <i class = "arrow down" id = "eventarrow" onclick = showhide("event")> </i ></div>
-                        <div >
-                            <div  class = "mnav_sub no_und" id = "mnav_event" style = "display: none; color: black">
-                                <a href="/events/lectures" id="nav_events_lectures" class = "nav_dd_sub">Lecture Series <br></a>
-                                <a href="/events/openhouse" id="nav_events_openhouse" class = "nav_dd_sub">Open Houses <br></a>
-                            </div>
-                        </div>
-                    
-                        <div class = "mnav_main "id="nav_photos">Photos <i class = "arrow down" id = "photoarrow" onclick = showhide("photo")> </i ></div>
-                        <div >
-                            <div class = "mnav_sub no_und " id = "mnav_photo" style = "display: none; color: black">
-                                <a href="/photos/photos" id="nav_photos_photos" class = "nav_dd_sub">Photo Gallery <br></a>
-                                <a href="/photos/museum" id="nav_photos_museum" class = "nav_dd_sub">Museum Gallery <br></a>
-                                <a href="/photos/astro" id="nav_photos_astro" class = "nav_dd_sub">Astrophotography <br></a>
-                            </div>
-                        </div>
-                        <div class = "no_und" > <a  class = "no_und" href="/newsletters"><div  id="nav_newsletters" class = "mnav_main ">Newsletters</div></a>   </div>
-               
-        
+                     <div class = "no_und" ><a class = "no_und"   href="/"><div  class = "mnav_main" id="nav_home">Home</div></a> </div>
+            
+            <div  class = "mnav_main" id="nav_about">About <i class = "arrow down" id = "aboutarrow" onclick = showhide("about")> </i > </div>
+                <div>
+                    <div class = "mnav_sub no_und"id = "mnav_about" style = "display: none; color: black">
+                    <br style = "font-size: 1vh">
+                        <a href="/about/theclub" id="nav_about_theclub" class = "nav_dd_sub">The Club <br> <br style = "font-size: 1vh"></a>
+                        <a href="/about/telescope" id="nav_about_telescope" class = "nav_dd_sub">The Telescope <br> <br style = "font-size: 1vh"></a>
+                        <a href="/about/fuertes" id="nav_about_fuertes" class = "nav_dd_sub">Fuertes History <br> <br style = "font-size: 1vh"></a>
+                        <a href="/about/irvhistory" id="nav_about_irvhistory" class = "nav_dd_sub">Telescope History <br> <br style = "font-size: 1vh"></a>
+                        <a href="/about/slides" id="nav_about_slides" class = "nav_dd_sub">Lantern Slide Wall <br> <br style = "font-size: 1vh"></a>
+                    </div>
+                </div>
+          
+ 
+                <div class = "mnav_main"  id="nav_visit">Visit Us <i class = "arrow down" id = "visitarrow" onclick = showhide("visit")> </i ></div>
+                <div>
+                    <div  class = "mnav_sub no_und" id = "mnav_visit" style = "display: none; color: black">
+                    <br style = "font-size: 1vh">
+                        <a href="/visit/hours" id="nav_visit_hours" class = "nav_dd_sub">Hours, Directions <br> <br style = "font-size: 1vh"></a>
+                        <a href="/visit/accessibility" id="nav_visit_accessibility" class = "nav_dd_sub">Accessibility <br> <br style = "font-size: 1vh"></a>
+                        <a href="/visit/weather" id="nav_visit_weather" class = "nav_dd_sub">Weather <br> <br style = "font-size: 1vh"></a>
+                    </div>
+                </div>
+
+                <div class = "mnav_main "  id="nav_events">Events <i class = "arrow down" id = "eventarrow" onclick = showhide("event")> </i ></div>
+                <div >
+                    <div  class = "mnav_sub no_und" id = "mnav_event" style = "display: none; color: black">
+                    <br style = "font-size: 1vh">
+                        <a href="/events/lectures" id="nav_events_lectures" class = "nav_dd_sub">Lecture Series <br> <br style = "font-size: 1vh"></a>
+                        <a href="/events/openhouse" id="nav_events_openhouse" class = "nav_dd_sub">Open Houses <br> <br style = "font-size: 1vh"></a>
+                    </div>
+                </div>
+            
+                <div class = "mnav_main "id="nav_photos">Photos <i class = "arrow down" id = "photoarrow" onclick = showhide("photo")> </i ></div>
+                <div >
+                    <div class = "mnav_sub no_und " id = "mnav_photo" style = "display: none; color: black">
+                    <br style = "font-size: 1vh">
+                        <a href="/photos/photos" id="nav_photos_photos" class = "nav_dd_sub">Photo Gallery <br> <br style = "font-size: 1vh"></a>
+                        <a href="/photos/museum" id="nav_photos_museum" class = "nav_dd_sub">Museum Gallery <br> <br style = "font-size: 1vh"></a>
+                        <a href="/photos/astro" id="nav_photos_astro" class = "nav_dd_sub">Astrophotography <br> <br style = "font-size: 1vh"></a>
+                    </div>
+                </div>
+                <div class = "no_und" > <a  class = "no_und" href="/newsletters"><div  id="nav_newsletters" class = "mnav_main ">Newsletters</div></a>   </div>
+       
                 </div> 
             </div>
         </div>`
