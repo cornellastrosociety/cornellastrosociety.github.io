6143cbeaae8f74dedd080912f2aa2eaa44291757
therealhashimbhatnagar <109086488+therealhashimbhatnagar@users.noreply.github.com>
2024-08-15 21:23:11 -0400

    change announcement location

--- data/js/status.js
+++ data/js/status.js
@@ -1,8 +1,8 @@
 var status_data = `<div style="width: 100%; background-color: rgba(140, 0, 0, 0.69); text-align: center; margin: auto; padding: 6vh 2vw; max-width: 100%; box-sizing: border-box; color: white">
-            <span style="text-shadow: white 0 0 6px; font-size: 40px;">Open every Friday <span style="font-weight: bold;">8 PM - midnight</span>.</span>
+            <span style="text-shadow: white 0 0 6px; font-size: 40px;">Open every Friday <span style="font-weight: bold;">8 PM - midnight</span>.<be> <span style = "font-weight: bold">We are not open on Friday, August 16th.</span></span>
             <br>
             <!-- Status message -->
-            <span style="text-shadow: white 0 0 4px; color: white; font-size: 24px;">Open all summer, but less staffed—check here in case of sudden cancellations. <br> We are not open on Friday, August 16th. </span>
+            <span style="text-shadow: white 0 0 4px; color: white; font-size: 24px;">Open all summer, but less staffed—check here in case of sudden cancellations.  </span>
         </div>`
 
 document.write(status_data);
