276f5c135701f9cebeb0937806c5d8362880ea38
ail36 <ail36@cornell.edu>
2024-08-16 23:56:05 +0100

    Clarify opening message

--- data/js/status.js
+++ data/js/status.js
@@ -2,7 +2,7 @@ var status_data = `<div style="width: 100%; background-color: rgba(140, 0, 0, 0.
             <span style="text-shadow: white 0 0 6px; font-size: 40px;"><span style="display: none;">Open every Friday <span style="font-weight: bold;">8 PM - midnight</span>.<br></span><span style = "font-weight: bold">We are not open on Friday, August 16th.</span></span>
             <br>
             <!-- Status message -->
-            <span style="text-shadow: white 0 0 4px; color: white; font-size: 24px;">No members of CAS are in Ithaca this weekend. However, we look forward to welcoming you to the observatory next week, from 8 PM to midnight!</span>
+            <span style="text-shadow: white 0 0 4px; color: white; font-size: 24px;">No members of CAS are in Ithaca this weekend. However, we look forward to welcoming you to the observatory next week, Friday night from 8 PM to midnight!</span>
         </div>`
 
 document.write(status_data);
