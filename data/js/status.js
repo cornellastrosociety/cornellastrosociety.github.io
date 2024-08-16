32eba43d27dc117f1df6e9221e747a000b8e6a9d
therealhashimbhatnagar <109086488+therealhashimbhatnagar@users.noreply.github.com>
2024-08-15 21:22:13 -0400

    update status (not open friday 16th)

--- data/js/status.js
+++ data/js/status.js
@@ -2,7 +2,7 @@ var status_data = `<div style="width: 100%; background-color: rgba(140, 0, 0, 0.
             <span style="text-shadow: white 0 0 6px; font-size: 40px;">Open every Friday <span style="font-weight: bold;">8 PM - midnight</span>.</span>
             <br>
             <!-- Status message -->
-            <span style="text-shadow: white 0 0 4px; color: white; font-size: 24px;">Open all summer, but less staffed—check here in case of sudden cancellations.</span>
+            <span style="text-shadow: white 0 0 4px; color: white; font-size: 24px;">Open all summer, but less staffed—check here in case of sudden cancellations. <br> We are not open on Friday, August 16th. </span>
         </div>`
 
 document.write(status_data);
