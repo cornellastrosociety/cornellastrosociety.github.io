4b00b3bfa7744ea87719c5cedaa23d796bf3e884
ail36 <ail36@cornell.edu>
2024-08-26 10:29:34 -0400

    Make status message generic

--- data/js/status.js
+++ data/js/status.js
@@ -1,8 +1,8 @@
 var status_data = `<div style="width: 100%; background-color: rgba(140, 0, 0, 0.69); text-align: center; margin: auto; padding: 6vh 2vw; max-width: 100%; box-sizing: border-box; color: white">
-            <span style="text-shadow: white 0 0 6px; font-size: 40px;">Open every Friday <span style="font-weight: bold;">8 PM - midnight</span>.</span>
-            <br>
+            <span style="text-shadow: white 0 0 6px; font-size: 40px;">Open every Friday <span style="font-weight: bold;">8 PM - midnight</span>!</span>
+            <!-- <br> -->
             <!-- Status message -->
-            <span style="text-shadow: white 0 0 4px; color: white; font-size: 24px;">Our first open house night of the Fall semester will be Friday, August 23rd!</span>
+            <!-- <span style="text-shadow: white 0 0 4px; color: white; font-size: 24px;">Our first open house night of the Fall semester will be Friday, August 23rd!</span> -->
         </div>`
 
 document.write(status_data);
