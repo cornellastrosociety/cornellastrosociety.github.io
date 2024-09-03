3b542a7cc319589829337ad995e8394ad9cfa757
ail36 <ail36@cornell.edu>
2024-09-03 11:54:18 -0400

    Remove special open house night message

--- data/js/status.js
+++ data/js/status.js
@@ -1,8 +1,8 @@
 var status_data = `<div style="width: 100%; background-color: rgba(140, 0, 0, 0.69); text-align: center; margin: auto; padding: 6vh 2vw; max-width: 100%; box-sizing: border-box; color: white">
             <span style="text-shadow: white 0 0 6px; font-size: 40px;">Open every Friday <span style="font-weight: bold;">8 PM - midnight</span>!</span>
-            <br>
+            <!-- <br> -->
             <!-- Status message -->
-            <span style="text-shadow: white 0 0 4px; color: white; font-size: 24px;">Open for a special open house night <span style="font-weight: bold;">September 2</span> from 7 PM to 6 AM.</span>
+            <!-- <span style="text-shadow: white 0 0 4px; color: white; font-size: 24px;">Open for a special open house night <span style="font-weight: bold;">September 2</span> from 7 PM to 6 AM.</span> -->
         </div>`
 
 document.write(status_data);
