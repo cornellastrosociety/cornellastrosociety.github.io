30afa61ae6b30acaf167151074842495e186b816
therealhashimbhatnagar <109086488+therealhashimbhatnagar@users.noreply.github.com>
2024-10-16 12:28:19 -0400

    Change status message

--- data/js/status.js
+++ data/js/status.js
@@ -2,7 +2,7 @@ var status_data = `<div style="width: 100%; background-color: rgba(140, 0, 0, 0.
             <span style="text-shadow: white 0 0 6px; font-size: 40px;">Open every Friday <span style="font-weight: bold;">8 PM - midnight</span>.</span>
             <br>
             <!-- Status message -->
-            <span style="text-shadow: white 0 0 4px; color: white; font-size: 24px;">We will be open the Friday preceding Fall Break, <span style="font-weight: bold;">October 11</span>, as usual.</span>
+            <span style="text-shadow: white 0 0 4px; color: white; font-size: 24px;">We will be open on Friday, <span style="font-weight: bold;">November 8</span>, from <span style="font-weight: bold;"> 8 PM to 11 PM </span> and on Saturday, <span style="font-weight: bold;">November 9</span>, from <span style="font-weight: bold;"> 10 PM to Midnight</span> to celebrate the legacy of Cornell professor Carl Sagan.</span>
         </div>`
 
 document.write(status_data);
