c1c2b7ebf9ec58a0d7d0435c8ff4739395253b12
therealhashimbhatnagar <109086488+therealhashimbhatnagar@users.noreply.github.com>
2024-10-16 12:33:06 -0400

    grammar issue

--- data/js/status.js
+++ data/js/status.js
@@ -2,7 +2,7 @@ var status_data = `<div style="width: 100%; background-color: rgba(140, 0, 0, 0.
             <span style="text-shadow: white 0 0 6px; font-size: 40px;">Open every Friday <span style="font-weight: bold;">8 PM - midnight</span>.</span>
             <br>
             <!-- Status message -->
-            <span style="text-shadow: white 0 0 4px; color: white; font-size: 24px;">We will close early on Friday, <span style="font-weight: bold;">November 8</span> at <span style="font-weight: bold;"> 11 PM </span>. To celebrate the legacy of Cornell professor Carl Sagan, we'll also be open on Saturday, <span style="font-weight: bold;">November 9</span>, from <span style="font-weight: bold;"> 10 PM to Midnight</span>.</span>
+            <span style="text-shadow: white 0 0 4px; color: white; font-size: 24px;">We will close early on Friday, <span style="font-weight: bold;">November 8</span> at <span style="font-weight: bold;"> 11 PM</span>. To celebrate the legacy of Cornell professor Carl Sagan, we'll also be open on Saturday, <span style="font-weight: bold;">November 9</span>, from <span style="font-weight: bold;"> 10 PM to Midnight</span>.</span>
         </div>`
 
 document.write(status_data);
