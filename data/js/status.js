37fd27f2a374e5cd3f5e45ab06da41891c06e63e
ail36 <ail36@cornell.edu>
2024-11-29 13:15:55 -0500

    Update thanksgiving message

--- data/js/status.js
+++ data/js/status.js
@@ -2,7 +2,7 @@ var status_data = `<div style="width: 100%; background-color: rgba(140, 0, 0, 0.
             <span style="text-shadow: white 0 0 6px; font-size: 40px;">Open every Friday <span style="font-weight: bold;">8 PM - midnight</span>.</span>
             <br>
             <!-- Status message -->
-            <span style="text-shadow: white 0 0 4px; color: white; font-size: 24px;">We will be open as usual Thanksgiving break!</span>
+            <span style="text-shadow: white 0 0 4px; color: white; font-size: 24px;">We will close early at 10 PM on Friday, November 29th.</span>
         </div>`
 
 document.write(status_data);
