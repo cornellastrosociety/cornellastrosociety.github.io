550235c9a8dde215ef57b3a9bf1944556b9da3c5
ail36 <ail36@cornell.edu>
2024-12-21 13:12:42 -0500

    Add closing message

--- data/js/status.js
+++ data/js/status.js
@@ -1,8 +1,8 @@
 var status_data = `<div style="width: 100%; background-color: rgba(140, 0, 0, 0.69); text-align: center; margin: auto; padding: 6vh 2vw; max-width: 100%; box-sizing: border-box; color: white">
-            <span style="text-shadow: white 0 0 6px; font-size: 40px;">Open every Friday <span style="font-weight: bold;">8 PM - midnight</span>.*</span>
+            <span style="text-shadow: white 0 0 6px; font-size: 40px;">We're closed for Winter Break.</span>
             <br>
             <!-- Status message -->
-            <span style="text-shadow: white 0 0 4px; color: white; font-size: 24px;">*Our final open house until January 17th will be December 20th.</span>
+            <span style="text-shadow: white 0 0 4px; color: white; font-size: 24px;">Friday night open houses will resume for January 17th, 2025, 8 PM to midnight!</span>
         </div>`
 
 document.write(status_data);
