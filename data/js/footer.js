fb2c607f8703050e0cf4c01842477458e94a4b51
ail36 <ail36@cornell.edu>
2024-12-30 01:11:04 -0500

    Shrink social icons; add bluesky

--- data/js/footer.js
+++ data/js/footer.js
@@ -12,10 +12,11 @@ var footer_data = `<div style="padding: 2vh;"></div>
         <div style="background-color: white; width: 100%;"></div>
         <div id="copyright" style="text-align: center; padding: 1rem 10%; font-size: 0.8em; line-height: 1.5em;">© 2024 by the Cornell Astronomical Society<br><a href="/members">Member Information</a><br><br>This organization is a registered student organization of Cornell University.</div>
         <div style="background-color: white; width: 100%;"></div>
-        <div style="padding: 1rem 10%; max-width: 20vw; display: grid; grid-template-columns: 1fr 1fr 1fr; gap: 5%; place-items: center;">
-            <a target="_blank" href="https://www.facebook.com/FuertesObservatory"><img style="width: 100%;" src="/data/social/Facebook_Logo_Primary.png"></a>
-            <a target="_blank" href="https://twitter.com/FuertesObs"><img style="width: 100%;" src="/data/social/Logo_of_Twitter.png"></a>
-            <a target="_blank" href="https://www.instagram.com/fuertesobservatory"><img style="width: 100%;" src="/data/social/Instagram_Glyph_Gradient.png"></a>
+        <div style="padding: 1rem 10%; max-width: 20vw; display: grid; grid-template-columns: 1fr 1fr 1fr 1fr; gap: 5%; place-items: center;">
+            <a target="_blank" href="https://www.facebook.com/FuertesObservatory"><img style="width: 100%;" src="/data/social/facebook.png"></a>
+            <a target="_blank" href="https://twitter.com/FuertesObs"><img style="width: 100%;" src="/data/social/twitter.png"></a>
+            <a target="_blank" href="https://www.instagram.com/fuertesobservatory"><img style="width: 100%;" src="/data/social/instagram.png"></a>
+            <a target="_blank" href="https://bsky.app/profile/cornellastrosociety.org"><img style="width: 100%;" src="/data/social/bluesky.png"></a>
         </div>
         <div></div>
     </div>
