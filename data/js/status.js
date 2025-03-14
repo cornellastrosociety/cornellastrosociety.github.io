ecca62ec67c0808994dae072e3de07c7cacb93f9
ail36 <ail36@cornell.edu>
2025-03-14 07:20:16 -0400

    Remove eclipse info

--- data/js/status.js
+++ data/js/status.js
@@ -2,7 +2,7 @@ var status_data = `<div style="width: 100%; background-color: rgba(140, 0, 0, 0.
             <span style="text-shadow: white 0 0 6px; font-size: 40px;">Open every Friday <span style="font-weight: bold;">8 PM - midnight</span>!</span>
             <br>
             <!-- Status message -->
-            <span style="text-shadow: white 0 0 4px; color: white; font-size: 24px;">We'll be hosting a special open house night throughout the night of March 13th, beginning at 10 PM, to observe the total lunar eclipse! See <a href="/#eventinfo">the home page</a> for more details.</span>
+            <span style="text-shadow: white 0 0 4px; color: white; font-size: 24px;">We'll be closing early the night of Friday, March 14th, at 10:00 PM.</span>
         </div>`
 
 document.write(status_data);
