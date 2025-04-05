9889a5a19581a58877c8e24f86a60fe2a3225b16
ail36 <ail36@cornell.edu>
2025-04-05 19:27:48 -0400

    Remove status message

--- data/js/status.js
+++ data/js/status.js
@@ -2,7 +2,6 @@ var status_data = `<div style="width: 100%; background-color: rgba(140, 0, 0, 0.
             <span style="text-shadow: white 0 0 6px; font-size: 40px;">Open every Friday <span style="font-weight: bold;">8 PM - midnight</span>!</span>
             <br>
             <!-- Status message -->
-            <span style="text-shadow: white 0 0 4px; color: white; font-size: 24px;">We'll be closing early the night of Friday, April 4th, at 11 PM.</span>
         </div>`
 
 document.write(status_data);
