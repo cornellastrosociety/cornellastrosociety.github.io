7973425ad9d8b75d1dcde77868173f8f1916e770
ail36 <ail36@cornell.edu>
2025-04-12 21:48:46 -0400

    Remove Yuri's Night info

--- data/js/status.js
+++ data/js/status.js
@@ -2,7 +2,7 @@ var status_data = `<div style="width: 100%; background-color: rgba(140, 0, 0, 0.
             <span style="text-shadow: white 0 0 6px; font-size: 40px;">Open every Friday <span style="font-weight: bold;">8 PM - midnight</span>!</span>
             <br>
             <!-- Status message -->
-            <span style="text-shadow: white 0 0 4px; color: white; font-size: 24px;">Yuri's Night is this Friday! Join us at 7 PM in Appel 303 for our annual lecture, presented this year by Professor Mason Peck. See the <a href="/events/lectures/">Lecture Series page</a> for more information.</span>
+            <!-- <span style="text-shadow: white 0 0 4px; color: white; font-size: 24px;">Yuri's Night is this Friday! Join us at 7 PM in Appel 303 for our annual lecture, presented this year by Professor Mason Peck. See the <a href="/events/lectures/">Lecture Series page</a> for more information.</span> -->
         </div>`
 
 document.write(status_data);
