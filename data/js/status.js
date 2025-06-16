be54cbd12a05230d53f564fa6921bdc5cd8197b9
Andrew Lewis <156634637+ail36@users.noreply.github.com>
2025-06-16 15:46:22 -0400

    Remove Reunion opening message

--- data/js/status.js
+++ data/js/status.js
@@ -1,8 +1,8 @@
 var status_data = `<div style="width: 100%; background-color: rgba(140, 0, 0, 0.69); text-align: center; margin: auto; padding: 6vh 2vw; max-width: 100%; box-sizing: border-box; color: white">
             <span style="text-shadow: white 0 0 6px; font-size: 40px;">Open every Friday <span style="font-weight: bold;">8 PM - midnight</span>!</span>
-            <br>
+            <!--<br>
             <!-- Status message -->
-            <span style="text-shadow: white 0 0 4px; color: white; font-size: 24px;">We will be hosting a special open house Saturday, June 7th, from 9 to 10:30 PM!</span>
+            <span style="text-shadow: white 0 0 4px; color: white; font-size: 24px;">We will be hosting a special open house Saturday, June 7th, from 9 to 10:30 PM!</span> -->
         </div>`
 
 document.write(status_data);
