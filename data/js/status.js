fa8422ae829f284d00a87c60806bc95c115b94e5
ail36 <ail36@cornell.edu>
2025-06-26 11:09:27 -0400

    Add July 4th closure message

--- data/js/status.js
+++ data/js/status.js
@@ -1,8 +1,8 @@
 var status_data = `<div style="width: 100%; background-color: rgba(140, 0, 0, 0.69); text-align: center; margin: auto; padding: 6vh 2vw; max-width: 100%; box-sizing: border-box; color: white">
-            <span style="text-shadow: white 0 0 6px; font-size: 40px;">Open every Friday <span style="font-weight: bold;">8 PM - midnight</span>!</span>
-            <!-- <br> -->
+            <span style="text-shadow: white 0 0 6px; font-size: 40px;">Open every Friday <span style="font-weight: bold;">8 PM - midnight</span>!<sup>*</sup></span>
+            <br>
             <!-- Status message -->
-            <!-- <span style="text-shadow: white 0 0 4px; color: white; font-size: 24px;">We will be hosting a special open house Saturday, June 7th, from 9 to 10:30 PM!</span> -->
+            <span style="text-shadow: white 0 0 4px; color: white; font-size: 24px;"><sup>*</sup>We will be <b>closed</b> the evening of July 4th, 2025.</span>
         </div>`
 
 document.write(status_data);
