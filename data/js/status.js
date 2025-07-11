4d291b0c22a6dd179500878ed9ffbca6a0f59bf8
ail36 <ail36@cornell.edu>
2025-07-10 20:30:34 -0400

    Add closing message

--- data/js/status.js
+++ data/js/status.js
@@ -1,8 +1,8 @@
 var status_data = `<div style="width: 100%; background-color: rgba(140, 0, 0, 0.69); text-align: center; margin: auto; padding: 6vh 2vw; max-width: 100%; box-sizing: border-box; color: white">
-            <span style="text-shadow: white 0 0 6px; font-size: 40px;">Open every Friday <span style="font-weight: bold;">8 PM - midnight</span>!</span>
-            <!-- <br> -->
+            <span style="text-shadow: white 0 0 6px; font-size: 40px;"><b>Open July 11th and 18th, 8 PM - midnight.</b></span>
+            <br>
             <!-- Status message -->
-            <!-- <span style="text-shadow: white 0 0 4px; color: white; font-size: 24px;"><sup>*</sup>We will be <b>open</b> the evening of July 4th, 2025, assuming observing conditions are satisfactory.</span> -->
+            <span style="text-shadow: white 0 0 4px; color: white; font-size: 24px;">Fuertes will be closed for following open houses as the dome undergoes repairs. We will provide updates here as soon as we have further information about progress and reopening!</span>
         </div>`
 
 document.write(status_data);
