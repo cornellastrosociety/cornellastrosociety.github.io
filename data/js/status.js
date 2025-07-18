f158773a0000873c5cb21853045c89bcd3a30d39
ail36 <ail36@cornell.edu>
2025-07-18 15:53:26 -0400

    Update status message

--- data/js/status.js
+++ data/js/status.js
@@ -1,8 +1,8 @@
 var status_data = `<div style="width: 100%; background-color: rgba(140, 0, 0, 0.69); text-align: center; margin: auto; padding: 6vh 2vw; max-width: 100%; box-sizing: border-box; color: white">
-            <span style="text-shadow: white 0 0 6px; font-size: 40px;"><b>Open July 11th and 18th, 8 PM - midnight.</b></span>
+            <span style="text-shadow: white 0 0 6px; font-size: 40px;"><b>Open July 18th, 8 PM - midnight.</b></span>
             <br>
             <!-- Status message -->
-            <span style="text-shadow: white 0 0 4px; color: white; font-size: 24px;">Fuertes will be closed for following open houses as the dome undergoes repairs. We will provide updates here as soon as we have further information about progress and reopening!</span>
+            <span style="text-shadow: white 0 0 4px; color: white; font-size: 24px;">Fuertes will not open on Friday, July 25th, and later, as the dome undergoes repairs. We will provide updates here as soon as we have further information about progress and reopening!</span>
         </div>`
 
 document.write(status_data);
