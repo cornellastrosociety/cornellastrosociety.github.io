be080d0225bba46525ca730e3795525fecbf8d4a
ail36 <ail36@cornell.edu>
2025-09-18 14:09:39 -0400

    Add dome closure message

--- data/js/status.js
+++ data/js/status.js
@@ -1,8 +1,9 @@
 var status_data = `<div style="width: 100%; background-color: rgba(140, 0, 0, 0.69); text-align: center; margin: auto; padding: 6vh 2vw; max-width: 100%; box-sizing: border-box; color: white">
             <span style="text-shadow: white 0 0 6px; font-size: 40px;">Open every Friday <span style="font-weight: bold;">8 PM - midnight</span>!</span>
             <br>
+            <br>
             <!-- Status message -->
-            <span style="text-shadow: white 0 0 4px; color: white; font-size: 24px;">Join our <a href="/#email">email list</a> for updates and open house information!</span>
+            <span style="text-shadow: white 0 0 4px; color: white; font-size: 18px; line-height: 1.5em;"><b>The deck and dome room will be closed</b> during our open house the evening of Friday, September 19th. Telescopes will be in use outside of the building, and the museum and slide wall will be open on the first floor of the building. As was done during the very first open houses at Fuertes in February of 1923, we will also make use of a glass slide projector with original glass slides to display antique photographs of astronomical objects!</span>
         </div>`
 
 document.write(status_data);
