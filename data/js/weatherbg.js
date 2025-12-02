f5c037b4c18ba42f5985431dad53634785739238
ail36 <ail36@cornell.edu>
2025-12-02 12:05:21 -0500

    Add additional weather backgrounds

--- data/js/weatherbg.js
+++ data/js/weatherbg.js
@@ -8,10 +8,18 @@ xmlhttp.onreadystatechange = function () {
         var temp = +tab[tab.findIndex(e => e.includes('temperature'))].match(/>\d+/g)[0].substring(1);
         var cloud = +tab[tab.findIndex(e => e.includes('cloud-amount'))].match(/>\d+/g)[0].substring(1);
         var precip = +tab[tab.findIndex(e => e.includes('probability-of-precipitation'))].match(/>\d+/g)[0].substring(1);
+        var precipavgsrw = tab[tab.findIndex(e => e.includes('probability-of-precipitation'))].match(/>\d+/g);
+        var precipavgs = []
+        for (let i = 0; i < 8; i++) precipavgs.push(+precipavgsrw[i].substring(1));
+        var precipavgs = precipavgs.reduce((a, i) => a + i, 0) / 8;
         var newclass = '';
-        if (precip >= 40 && temp <= 32) newclass = 'snow_bg';
+        if (precipavgs >= 80 && temp <= 20 && new Date().getDay() == 5 && new Date().getHours() >= 17) newclass = 'idiot_bg';
+        else if (precipavgs >= 70 && temp <= 25 && [0, 5, 6].includes(new Date().getDay()) && (new Date().getHours() >= 17 || new Date().getHours < 6)) newclass = 'sled_bg';
+        else if (precip >= 40 && temp <= 32) newclass = 'snow_bg';
         else if (cloud >= 75) newclass = 'cloud_bg';
         else if (cloud >= 25) newclass = 'pc_bg';
+        else if (cloud >= 17) newclass = 'sc_bg';
+        else if (cloud >= 10) newclass = 'mc_bg';
         else newclass = 'clear_bg';
         document.getElementsByTagName('body')[0].classList.add(newclass);
     }
