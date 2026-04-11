f527fcdc02c488dee9806fc8548a70ea811bc20f
ail36 <ail36@cornell.edu>
2026-04-11 01:47:27 -0400

    Remove Yuri's Night from status

--- data/js/status.js
+++ data/js/status.js
@@ -1,11 +1,10 @@
 var status_data = `<div style="width: 100%; background-color: rgba(140, 0, 0, 0.69); text-align: center; margin: auto; padding: 6vh 2vw; max-width: 100%; box-sizing: border-box; color: white">
             <span style="text-shadow: white 0 0 6px; font-size: 40px;">Open every Friday <span style="font-weight: bold;">8 PM - midnight</span>!</span>
             <br>
-            <img alt="Yuri's Night sticker by Winnie Chen" style="padding: 4vh; max-width: 25%;" src="/data/img/yurisnight.png">
-            <br>
+            <!-- <img alt="Yuri's Night sticker by Winnie Chen" style="padding: 4vh; max-width: 25%;" src="/data/img/yurisnight.png"> -->
             <!-- <br> -->
             <!-- Status message -->
-            <span style="text-shadow: white 0 0 4px; color: white; font-size: 24px; line-height: 1.5em;"><a href="/events/lectures">Yuri's Night</a> takes place Friday, April 10th, in Appel 303 at 7 PM with Josh Umansky-Castro! <b>Please sign up <a target="_blank" href="https://cglink.me/2ee/r2303642">here</a>!</b></span>
+            <!-- <span style="text-shadow: white 0 0 4px; color: white; font-size: 24px; line-height: 1.5em;"><a href="/events/lectures">Yuri's Night</a> takes place Friday, April 10th, in Appel 303 at 7 PM with Josh Umansky-Castro! <b>Please sign up <a target="_blank" href="https://cglink.me/2ee/r2303642">here</a>!</b></span> -->
         </div>`
 
 document.write(status_data);
