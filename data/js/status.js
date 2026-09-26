ccbfd89234664dc966fc53990c5ea986ad710e37
ail36 <ail36@cornell.edu>
2026-09-26 12:46:32 -0400

    Update status text

--- data/js/status.js
+++ data/js/status.js
@@ -4,9 +4,6 @@ var status_data = `<div style="width: 100%; background-color: rgba(140, 0, 0, 0.
             <!-- <img alt="Yuri's Night sticker by Winnie Chen" style="padding: 4vh; max-width: 25%;" src="/data/img/yurisnight.png"> -->
             <br>
             <!-- Status message -->
-            <span style="text-shadow: white 0 0 4px; color: white; font-size: 24px; line-height: 1.5em;">CAS's second lecture of the semester will be September 25th! View details <a href="/events/lectures/">here</a>.</span>
-            <br>
-            <br>
             <span style="text-shadow: white 0 0 4px; color: white; font-size: 24px; line-height: 1.5em;">Interested in joining CAS, or would like updates about CAS events? Join our mailing list <a href="#email">here</a> for information about open houses, club meetings, lectures, and special events!</span>
         </div>`
 
